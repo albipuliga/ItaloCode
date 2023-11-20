9aa4d8f78d9a461070d0717b46889f5a133a496a
Alberto Puliga <albi.puliga@gmail.com>
2023-11-20 22:10:53 +0100

    updated gitignore and repo structureø

--- src/lexer.c
+++ src/lexer.c
@@ -28,25 +28,23 @@ typedef enum {
 } TokenType;
 
 
-
-
 // given an idex it will return the name of the unum token
-const char* getTokenTypeName(TokenType type) {
-    const char* typeNames[] = {
-        "TOKEN_KEYWORD",
-        "TOKEN_DATATYPE",
-        "TOKEN_ARITHMETIC_OP",
-        "TOKEN_COMPARISON_OP",
-        "TOKEN_LOGICAL_OP",
-        "TOKEN_PUNCTUATION",
-        "TOKEN_BRACE",
-        "SINGLE_LINE_COMMENT",
-        "TOKEN_IDENTIFIER",
-        "TOKEN_NUMBER",    
-        "TOKEN_FOR_LOOP",         
-        "TOKEN_WHILE_LOOP",  
-        "DATA_TYPE",        
-        "TOKEN_EOF"
+const char *getTokenTypeName(TokenType type) {
+    const char *typeNames[] = {
+            "TOKEN_KEYWORD",
+            "TOKEN_DATATYPE",
+            "TOKEN_ARITHMETIC_OP",
+            "TOKEN_COMPARISON_OP",
+            "TOKEN_LOGICAL_OP",
+            "TOKEN_PUNCTUATION",
+            "TOKEN_BRACE",
+            "SINGLE_LINE_COMMENT",
+            "TOKEN_IDENTIFIER",
+            "TOKEN_NUMBER",
+            "TOKEN_FOR_LOOP",
+            "TOKEN_WHILE_LOOP",
+            "DATA_TYPE",
+            "TOKEN_EOF"
     };
 
     if (type >= TOKEN_KEYWORD && type <= TOKEN_EOF) {
@@ -57,77 +55,76 @@ const char* getTokenTypeName(TokenType type) {
 }
 
 
-
-
 // Token structure
 typedef struct {
     TokenType type;
-    char* lexeme;
+    char *lexeme;
 } Token;
 
 
-
-
 // Function to check if a string is a keyword and return its token type
-TokenType checkKeyword(char* str) {
+TokenType checkKeyword(char *str) {
     // Check for Keywords
-    if (strcmp(str, "CIAO") == 0 || strcmp(str, "PASTA") == 0 || strcmp(str, "ALDENTE") == 0 /* ... other keywords ... */) {
+    if (strcmp(str, "CIAO") == 0 || strcmp(str, "PASTA") == 0 ||
+        strcmp(str, "ALDENTE") == 0 /* ... other keywords ... */) {
         return TOKEN_KEYWORD;
     }
-    // Check for Data Types
-    else if (strcmp(str, "CARBS") == 0 || strcmp(str, "VEGGIE") == 0 || strcmp(str, "FORMAGGIO") == 0 || strcmp(str, "OLIO") == 0) {
+        // Check for Data Types
+    else if (strcmp(str, "CARBS") == 0 || strcmp(str, "VEGGIE") == 0 || strcmp(str, "FORMAGGIO") == 0 ||
+             strcmp(str, "OLIO") == 0) {
         return TOKEN_DATATYPE;
     }
-    // Check for Logical Operators
+        // Check for Logical Operators
     else if (strcmp(str, "E") == 0 || strcmp(str, "OPPURE") == 0) {
         return TOKEN_LOGICAL_OP;
     }
-    // Check for Arithmetic Operators
+        // Check for Arithmetic Operators
     else if (strcmp(str, "+") == 0 || strcmp(str, "-") == 0 || strcmp(str, "*") == 0 || strcmp(str, "/") == 0) {
         return TOKEN_ARITHMETIC_OP;
     }
-    // Check for Comparison Operators
+        // Check for Comparison Operators
     else if (strcmp(str, "!=") == 0 || strcmp(str, "<") == 0 || strcmp(str, ">") == 0 || strcmp(str, "=") == 0) {
         return TOKEN_COMPARISON_OP;
     }
-    // Check for Punctuation
-    else if (strcmp(str, ";") == 0 || strcmp(str, ",") == 0 || strcmp(str, ":") == 0 || strcmp(str, "|") == 0 || strcmp(str, "\"") == 0) {
+        // Check for Punctuation
+    else if (strcmp(str, ";") == 0 || strcmp(str, ",") == 0 || strcmp(str, ":") == 0 || strcmp(str, "|") == 0 ||
+             strcmp(str, "\"") == 0) {
         return TOKEN_PUNCTUATION;
     }
-    // Check for Numbers
+        // Check for Numbers
     else if (strspn(str, "0123456789") == strlen(str)) {
         return TOKEN_NUMBER;
     }
-    // Check for "for" loop
+        // Check for "for" loop
     else if (strcmp(str, "for") == 0) {
         return TOKEN_FOR_LOOP;
     }
-    // Check for "while" loop
+        // Check for "while" loop
     else if (strcmp(str, "while") == 0) {
         return TOKEN_WHILE_LOOP;
     }
-    // Check for Single Line Comment
+        // Check for Single Line Comment
     else if (strncmp(str, "//", 2) == 0) {
         return SINGLE_LINE_COMMENT;
     }
-    // Check for Brace
-    else if (strcmp(str, "[") == 0 || strcmp(str, "]") == 0 || strcmp(str, "{") == 0 || strcmp(str, "}") == 0 || strcmp(str, "(") == 0 || strcmp(str, ")") == 0 ) {
+        // Check for Brace
+    else if (strcmp(str, "[") == 0 || strcmp(str, "]") == 0 || strcmp(str, "{") == 0 || strcmp(str, "}") == 0 ||
+             strcmp(str, "(") == 0 || strcmp(str, ")") == 0) {
         return TOKEN_BRACE;
     }
-    // Check for Data Types
-    else if (strcmp(str, "int") == 0 || strcmp(str, "float") == 0 ) {
+        // Check for Data Types
+    else if (strcmp(str, "int") == 0 || strcmp(str, "float") == 0) {
         return DATA_TYPE;
     }
-    // Default to Identifier
+        // Default to Identifier
     else {
         return TOKEN_IDENTIFIER;
     }
 }
 
 
-
 // Function to tokenize a single word
-Token tokenizeWord(char* word) {
+Token tokenizeWord(char *word) {
     Token token;
     token.type = checkKeyword(word);
     token.lexeme = strdup(word);
@@ -135,19 +132,18 @@ Token tokenizeWord(char* word) {
 }
 
 
-
 /*BEST LEXER*/
-void lexer(const char* input) {
+void lexer(const char *input) {
     const char delimiters[] = " \t\n"; // Word delimiters
-    char* copy = strdup(input); // Make a copy of the input string
-    char* word = strtok(copy, delimiters); // Tokenize the first word
+    char *copy = strdup(input); // Make a copy of the input string
+    char *word = strtok(copy, delimiters); // Tokenize the first word
 
     while (word != NULL) {
         // Tokenize the current word
         Token token = tokenizeWord(word);
 
         // Get the token type name
-        const char* typeName = getTokenTypeName(token.type);
+        const char *typeName = getTokenTypeName(token.type);
 
         // Print the token information
         printf(" Lexeme: %s, TokenName: %s, Token: %d\n", token.lexeme, typeName, token.type);
@@ -159,9 +155,9 @@ void lexer(const char* input) {
     // Free the memory allocated for the copy
     free(copy);
 }
- 
+
 int main() {
-    const char* input = "for ( int i = 0 ; i < 10 ; i + + ) { CIAO } while ( x > 0 ) { PASTA } 3 4 34";
+    const char *input = "for ( int i = 0 ; i < 10 ; i + + ) { CIAO } while ( x > 0 ) { PASTA } 3 4 34";
     lexer(input);
 
     return 0;
